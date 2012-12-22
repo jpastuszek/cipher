95429deaf97130db042daa1a57be413c7a1e441e
Jakub Pastuszek <jpastuszek@gmail.com>
2012-12-22 19:01:50 +0100

    loading of SDL header envelope

--- lib/cipher/envelope.rb
+++ lib/cipher/envelope.rb
@@ -1,6 +1,16 @@
 require 'sdl4r'
 
 class Envelope
+	class Sink
+		def initialize(&sink)
+			@sink = sink
+		end
+
+		def <<(data)
+			@sink.call data
+		end
+	end
+
 	class SDL
 		class HeaderDSL
 			def initialize(&dsl)
@@ -27,16 +37,6 @@ class Envelope
 			end
 		end
 
-		class Sink
-			def initialize(&sink)
-				@sink = sink
-			end
-
-			def <<(data)
-				@sink.call data
-			end
-		end
-
 		def initialize(&header)
 			@header = HeaderDSL.new(&header).header
 		end
@@ -49,6 +49,46 @@ class Envelope
 
 		def body
 			yield @sink
+		end
+
+		class Loader
+			def on_header(&callback)
+				@on_header = callback
+				self
+			end
+
+			def on_body(&callback)
+				@on_body = callback
+				self
+			end
+
+			def on_end(&callback)
+				@on_end = callback
+				self
+			end
+
+			def load
+				header = nil
+				header_data = ''
+
+				sink = Sink.new do |data|
+					unless header
+						header_data << data
+						if header_data.include? "\n\n"
+							header, data = header_data.split("\n\n", 2)
+							@on_header.call(SDL4R.load(header)) if @on_header
+							@on_body.call(data) if @on_body
+						end
+					else
+						@on_body.call(data) if @on_body
+					end
+				end
+
+				yield sink
+
+				@on_end.call if @on_end
+				self
+			end
 		end
 	end
 end
--- spec/envelope_spec.rb
+++ spec/envelope_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/cipher/envelope'
 
 describe Envelope::SDL do
-	it 'should allow defining header values' do
+	it 'should generate message' do
 		message = ''
 
 		Envelope::SDL.new do
@@ -23,6 +23,42 @@ key_length 128
 
 testtesttesttest
 EOF
+	end
+
+	it 'should load streamed message' do
+		header = nil
+		body = ''
+		test_message = <<EOF.chomp
+cipher "AES"
+mode "CBC"
+key_length 128
+
+testtesttesttest
+
+hello
+EOF
+
+		Envelope::SDL::Loader.new
+		.on_header do |h|
+			header = h
+		end
+		.on_body do |data|
+			body << data
+		end
+		.on_end do
+			body << '.'
+		end
+		.load do |sink|
+			# put message chunk by chunk
+			test_message.chars.each_slice(3) do |slice|
+				sink << slice.join
+			end
+		end
+
+		header.cipher.should == 'AES'
+		header.mode.should == 'CBC'
+		header.key_length.should == 128
+		body.should == "testtesttesttest\n\nhello."
 	end
 end
 
