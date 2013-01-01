af83910a698e21dbc66399281740656ad16802cc
Jakub Pastuszek <jpastuszek@gmail.com>
2013-01-01 15:27:41 +0100

    reimplemented filter chaing in filter class

--- lib/cipher/filter.rb
+++ lib/cipher/filter.rb
@@ -26,11 +26,11 @@ class Filter
 	def input
 		fail 'filter output not connected' unless @processor
 
-		@processor.output @header_input.call if @header_input
 		@on_start and @on_start.each{|callback| callback.call}
+		@processor.output @header_input.call if @header_input
 		yield @processor
-		@on_end and @on_end.each{|callback| callback.call}
 		@processor.output @footer_input.call if @footer_input
+		@on_end and @on_end.each{|callback| callback.call}
 		self
 	end
 
@@ -52,6 +52,25 @@ class Filter
 	def on_end(&callback)
 		(@on_end ||= []) << callback
 		self
+	end
+
+	def chain(filter)
+		@chain_fiber = Fiber.new do |input_data|
+			filter.input do |input|
+				begin
+					input << input_data
+				end while input_data = Fiber.yield
+			end
+		end
+
+		output do |output|
+			@chain_fiber.resume output if @chain_fiber
+		end
+
+		on_end do
+			@chain_fiber.resume nil
+			@chain_fiber = nil
+		end
 	end
 end
 
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -54,5 +54,70 @@ describe Filter do
 
 		out.should == 'HELLO WORLD <<'
 	end
+
+	describe 'chaining' do
+		it 'should allow chaining of filters' do
+			upcase_filter = Filter.new do |input|
+				input.upcase
+			end
+
+			space_filter = Filter.new do |input|
+				input + ' '
+			end
+
+			out = ''
+
+			upcase_filter.chain(space_filter)
+
+
+			space_filter.output do |output|
+				out << output
+			end
+
+			upcase_filter.input do |input|
+				input << 'hello'
+				input << 'world'
+			end
+
+			out.should == 'HELLO WORLD '
+		end
+
+		it 'should call all header and footer handlers' do
+			upcase_filter = Filter.new do |input|
+				input.upcase
+			end
+			upcase_filter.header do
+				'aa'
+			end
+			upcase_filter.footer do
+				'bb'
+			end
+
+			space_filter = Filter.new do |input|
+				'(' + input + ')'
+			end
+			space_filter.header do
+				'xx'
+			end
+			space_filter.footer do
+				'zz'
+			end
+
+			upcase_filter.chain(space_filter)
+
+			out = ''
+
+			space_filter.output do |output|
+				out << output
+			end
+
+			upcase_filter.input do |input|
+				input << 'hello'
+				input << 'world'
+			end
+
+			out.should == 'xx(aa)(HELLO)(WORLD)(bb)zz'
+		end
+	end
 end
 
