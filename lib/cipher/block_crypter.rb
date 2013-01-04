760325a6a3a305603f58189fa23c81d799f3ecff
Jakub Pastuszek <jpastuszek@gmail.com>
2013-01-04 11:38:20 +0100

    cleanup

--- lib/cipher/block_crypter.rb
+++ lib/cipher/block_crypter.rb
@@ -13,17 +13,12 @@ module BlockCipher
 
 				shift_register = @initialization_vector
 				input_sub_block = nil
-				last_block_length = nil
 
 				@log.debug "Using initialization vector: #{initialization_vector.to_hex}"
 
 				# save input and send shift_register to crypter
 				super() do |input, output|
 					input_sub_block = input
-
-					# save size of last block if smaller that sub block
-					last_block_length = input.length if input.length < sub_block_length
-
 					output << shift_register
 				end
 
@@ -36,24 +31,11 @@ module BlockCipher
 				xor_shift = Filter.new do |crypt_block, output|
 					crypt_block_sub_block = crypt_block.byteslice(0, sub_block_length)
 					ciphertext = crypt_block_sub_block ^ input_sub_block
-
-					unless last_block_length
-						output << ciphertext
-					else
-						# strip bytes not needed for decryption - no paddning needed in this mode
-						output << ciphertext.byteslice(0, last_block_length)
-					end
-
-					shift_register = shift_register.lshift(ciphertext.byteslice(0, sub_block_length))
+					ciphertext = ciphertext.byteslice(0, input_sub_block.length)
+					output << ciphertext
+					shift_register = shift_register.lshift(ciphertext)
 				end
 				crypter.nest(xor_shift)
-
-				# add zero padding if needed - this is not needed at all!?
-				#if options[:padding] 
-				#	footer do
-				#		0.chr(Encoding::ASCII_8BIT) * sub_block_length if padding_length == 0
-				#	end
-				#end
 			end
 
 			attr_reader :initialization_vector
